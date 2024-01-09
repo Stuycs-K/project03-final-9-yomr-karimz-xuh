6aaa37447c4ba1201de49b8dc850c5ffc0b17677
pyshrekek <rukereero@gmail.com>
2024-01-09 14:50:23 -0500

    scoring revamp

--- client.c
+++ client.c
@@ -41,56 +41,63 @@ int pointSystem(struct questionAndOptions* question, char* playerAnswer, struct 
 
 void clientLogic(int server_socket){
 
-  char question_buffer[BUFFER_SIZE];
-  char response_buffer[BUFFER_SIZE];
-  int bytes_read;
-  int current_question_number = 0;
+    struct questionAndOptions * question_buffer;
+    char response_buffer[BUFFER_SIZE];
+    int score = 0;
 
-  // wait for game to start, block until server sends a question
-  printf("Waiting for game to start...");
-  bytes_read = read(server_socket, question_buffer, sizeof(question_buffer));
-  if (bytes_read <= 0) {
-    printf("server disconnected\n");
-    exit(0);
-  }
+    int bytes_read;
+    int current_question_number = 0;
 
-  // once server sends the first question, loop
-  while (1) {
-    // read question
+    // wait for game to start, block until server sends a question
+    printf("Waiting for game to start...");
     bytes_read = read(server_socket, question_buffer, sizeof(question_buffer));
     if (bytes_read <= 0) {
-      printf("server disconnected\n");
-      exit(0);
+        printf("server disconnected\n");
+        exit(0);
     }
 
-    // handle game ending signal
-    signal(SIGUSR1, game_end);
+  // once server sends the first question, loop
+    while (1) {
+        // read question
+        bytes_read = read(server_socket, question_buffer, sizeof(question_buffer));
+        if (bytes_read <= 0) {
+            printf("server disconnected\n");
+            exit(0);
+        }
 
-    int i = 0;
-    printf("Question %d: %s\n", current_question_number, question_buffer); // print question to client
-    fgets(response_buffer, sizeof(response_buffer), stdin); // read client response from command line
-    while (response_buffer[i]) {
-      if (response_buffer[i] == '\n') response_buffer[i] = '\0';
-      i++;
+        // handle game ending signal
+        signal(SIGUSR1, game_end);
+
+        struct timeval start_time;
+        gettimeofday(&start_time, NULL);
+
+        int i = 0;
+        printf("Question %d: %s\n", current_question_number, question_buffer->question); // print question to client
+        fgets(response_buffer, sizeof(response_buffer), stdin); // read client response from command line
+        while (response_buffer[i]) {
+            if (response_buffer[i] == '\n') response_buffer[i] = '\0';
+            i++;
+        }
+
+        int add_points = pointSystem(question_buffer, response_buffer, start_time);
+        score = score + add_points;
+
+        write(server_socket, score, sizeof(response_buffer)); // write the client's total score back to the server
+
+        current_question_number++;
     }
-    write(server_socket, response_buffer, sizeof(response_buffer)); // write the response back to the server
-
-    current_question_number++;
-  }
 
   // display final scores at the end
-
-
 
   close(server_socket);
 }
 
 int main(int argc, char *argv[] ) {
-  char* IP = "127.0.0.1";
-  if(argc>1){
-    IP=argv[1];
-  }
-  int server_socket = client_tcp_handshake(IP);
-  printf("client connected.\n");
-  clientLogic(server_socket);
+    char* IP = "127.0.0.1";
+    if(argc>1){
+        IP=argv[1];
+    }
+    int server_socket = client_tcp_handshake(IP);
+    printf("client connected.\n");
+    clientLogic(server_socket);
 }
