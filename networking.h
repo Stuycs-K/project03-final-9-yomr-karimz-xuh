2f806d26af08a55814bac1929f8da0f76cbe4d45
Zidane Karim <zkarim40@stuy.edu>
2024-01-10 14:50:19 -0500

    max players to 2

--- networking.h
+++ networking.h
@@ -21,7 +21,7 @@
 #ifndef NETWORKING_H
 #define NETWORKING_H
 #define PORT "19230"
-#define MAX_PLAYERS 1
+#define MAX_PLAYERS 2
 #define BUFFER_SIZE 1024
 void err(int i, char * message);
 int server_setup();
